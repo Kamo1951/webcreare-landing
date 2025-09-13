15087b1e7f48eef5b85c51fd5f9b31b9c78f44a3
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-09-13 16:30:07 +0200

    add structure for layout with header, nav, and aside elements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <aside></aside>
+        <header>
+          <nav></nav>
+        </header>
+        <main>{children}</main>
       </body>
     </html>
   );
