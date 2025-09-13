dd0ef59d7a2a0d1cf35153d932b94b82a4a7e5df
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-09-13 17:05:11 +0200

    update Home component to include Navbar for improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import Navbar from "./components/navbar/Navbar";
 
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  );
 }
