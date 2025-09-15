b318481ccc29bfe4b1125167e8baa6550dcd1ee5
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-09-15 19:24:06 +0200

    feat: add hero shape image to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/navbar/Navbar";
+import HeroShape from "./components/navbar/imgs/hero-shape-5.png";
 
 export default function Home() {
   return (
     <>
       <div>
+        <div className="absolute">
+          <Image src={HeroShape} alt="Logo" width={1200} height={1200} />
+        </div>
         <Navbar />
       </div>
     </>
