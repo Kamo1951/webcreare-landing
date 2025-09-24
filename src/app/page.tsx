c7d67c7da4bbdc7196011410f79a79786b75d715
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-09-24 20:21:37 +0200

    refactor: clean up Home component structure and improve image handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,41 @@
 import Image from "next/image";
 import Navbar from "./components/navbar/Navbar";
 import HeroShape from "./components/navbar/imgs/hero-shape-5.png";
+import HeroCircle from "./components/navbar/imgs/hero-circle.png";
+import { Kontakt } from "./components/buttons/Kontakt";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <div className="absolute">
-          <Image src={HeroShape} alt="Logo" width={1200} height={1200} />
+      <div className="absolute">
+        <Image
+          src={HeroShape}
+          alt="Logo"
+          width={1200}
+          height={1200}
+          className="-z-10"
+        />
+      </div>
+      <Navbar />
+      <div className="">
+        {/* <Image src={HeroCircle} /> */}
+        <div className="">
+          <div>
+            <p>Lassen Sie</p>
+          </div>
+          <div>
+            <p>
+              <b>
+                Ihre
+                <span className="text-[var(--accent-color)]"> Webseite</span>
+              </b>
+            </p>
+          </div>
+          <div>
+            <p>Erstellen</p>
+          </div>
         </div>
-        <Navbar />
-        <div className="h-[2000px]"></div>
+        <Kontakt />
       </div>
     </>
   );
