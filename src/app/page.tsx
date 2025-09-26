dca3546f48c468c094094ba8a5840cb60f63f85b
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-09-26 13:23:04 +0200

    refactor: update layout and z-index values in Home component for improved visual hierarchy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,23 @@ import { HeroTitle } from "./components/hero/HeroTitle";
 export default function Home() {
   return (
     <>
-      <div className="absolute select-none -z-10">
+      <div className="absolute select-none -z-2">
         <Image src={HeroShape} alt="Logo" width={1200} height={1200} />
       </div>
       <Navbar />
-      <section className="container mx-auto px-4">
-        <div className="absolute select-none inset-50 ml-100 -z-9 rounded-full w-[500px] h-[500px] bg-red-500"></div>
+      <section className="container mx-auto px-4 overflow-hidden-x">
+        <div
+          className="absolute select-none -mt-200 ml-100 -z-1 rounded-full w-[1200px] h-[1200px] blur-3xl opacity-30
+          bg-[radial-gradient(circle_at_60%_40%,rgba(255,255,255,0.12)_0%,rgba(228,228,228,0.12)_28%,rgba(228,228,228,0.06)_52%,rgba(228,228,228,0)_80%)]
+          "
+        />
+        <div
+          className="absolute select-none -mt-10 ml-30 -z-3 rounded-full w-[700px] h-[700px] bg-[var(--background-box-color)]
+          "
+        />
         <HeroTitle />
       </section>
+      <div className="h-[2000px]"></div>
     </>
   );
 }
