5337e9fcbb732088cfcca8ee97a4f14976a79b54
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-09-26 12:47:59 +0200

    refactor: clean up Home component by removing unused elements and optimizing layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,39 +2,18 @@ import Image from "next/image";
 import Navbar from "./components/navbar/Navbar";
 import HeroShape from "./components/navbar/imgs/hero-shape-5.png";
 import HeroCircle from "./components/navbar/imgs/hero-circle.png";
-import { Kontakt } from "./components/buttons/Kontakt";
+import { HeroTitle } from "./components/hero/HeroTitle";
 
 export default function Home() {
   return (
     <>
-      <div className="absolute">
-        <Image
-          src={HeroShape}
-          alt="Logo"
-          width={1200}
-          height={1200}
-          className="-z-10 select-none"
-        />
+      <div className="absolute select-none -z-10">
+        <Image src={HeroShape} alt="Logo" width={1200} height={1200} />
       </div>
       <Navbar />
       <section className="container mx-auto px-4">
-        <div className="mt-12 sm:mt-16 md:mt-20 lg:mt-24 ">
-          <div className="leading-snug">
-            <h2 className="font-extralight text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-8xl">
-              Lassen Sie
-            </h2>
-            <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-[9rem]">
-              Ihre
-              <span className="text-[var(--accent-color)]"> Webseite</span>
-            </h1>
-            <h3 className="font-extralight text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-8xl">
-              Erstellen
-            </h3>
-          </div>
-          <div className="mt-8 sm:mt-10">
-            <Kontakt />
-          </div>
-        </div>
+        <div className="absolute select-none inset-50 ml-100 -z-9 rounded-full w-[500px] h-[500px] bg-red-500"></div>
+        <HeroTitle />
       </section>
     </>
   );
