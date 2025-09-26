5e95b4f0c3912da5c736d2a422faefef767c274d
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-09-26 16:25:13 +0200

    fix: update HeroCircle image source and improve Image component attributes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,29 @@
 import Image from "next/image";
 import Navbar from "./components/navbar/Navbar";
 import HeroShape from "./components/navbar/imgs/hero-shape-5.png";
-import HeroCircle from "./components/navbar/imgs/hero-circle.png";
+import HeroCircle from "./components/navbar/imgs/hero-shape-6.png";
 import { HeroTitle } from "./components/hero/HeroTitle";
 
 export default function Home() {
   return (
     <>
       <div className="absolute select-none -z-2">
-        <Image src={HeroShape} alt="Logo" width={1200} height={1200} />
+        <Image
+          src={HeroShape}
+          alt="Hero-Shape"
+          width={1200}
+          height={1200}
+          loading="lazy"
+        />
       </div>
       <Navbar />
       <section className="container mx-auto px-4 overflow-hidden-x">
-        <div
-          className="absolute select-none -mt-200 ml-100 -z-1 rounded-full w-[1200px] h-[1200px] blur-3xl opacity-30
-          bg-[radial-gradient(circle_at_60%_40%,rgba(255,255,255,0.12)_0%,rgba(228,228,228,0.12)_28%,rgba(228,228,228,0.06)_52%,rgba(228,228,228,0)_80%)]
-          "
-        />
-        <div
-          className="absolute select-none -mt-10 ml-30 -z-3 rounded-full w-[700px] h-[700px] bg-[var(--background-box-color)]
-          "
-        />
+        <Image
+          src={HeroCircle}
+          alt="Hero-Shape"
+          loading="lazy"
+          className="absolute -mt-20 -ml-40"
+        ></Image>
         <HeroTitle />
       </section>
       <div className="h-[2000px]"></div>
