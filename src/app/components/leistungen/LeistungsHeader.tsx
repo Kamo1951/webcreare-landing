029da2c94bf6809888cd683b31aec5414afcaa12
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-09-27 14:38:43 +0200

    fix: remove empty div and ensure proper structure in LeistungsHeader component

--- src/app/components/leistungen/LeistungsHeader.tsx
+++ src/app/components/leistungen/LeistungsHeader.tsx
@@ -1,7 +1,14 @@
 export function LeistungsHeader() {
   return (
     <>
-      <div></div>
+      <div className="">
+        <div className="flex justify-center content-center">
+          <span className="bg-gradient-to-r from-[rgba(63,90,243,0)] to-[rgb(63,90,243,40)] w-8 h-2 "></span>
+          <h4 className="uppercase px-2 leistungen">Unsere Leistungen</h4>
+          <span></span>
+        </div>
+        <h2 className="capitalize">Wir helfen beim Wachstum Ihrer Marke</h2>
+      </div>
     </>
   );
 }
