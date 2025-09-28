cb7a27e682510e28f3eb277f11b727635612e813
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-09-28 20:12:26 +0200

    feat: add KeywordCarousel component to display a list of keywords

---
+++ src/app/components/carousel/KeywordCarousel.tsx
@@ -0,0 +1,15 @@
+export function KeywordCarousel(keyword: string[]) {
+  return (
+    <>
+      <ul className="uppercase">
+        <li>Entwicklug</li>
+        <li>Design</li>
+        <li>Markenbildung</li>
+        <li>Digitale Lösungen</li>
+        <li>Ideen</li>
+        <li>Strategie</li>
+        <li>Kreative Lösungen</li>
+      </ul>
+    </>
+  );
+}
