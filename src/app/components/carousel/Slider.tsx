09bc27e189fa182c6649f13cb448f80abe5c086e
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-09-28 20:41:52 +0200

    feat: implement Slider component with props for id, text, and direction

---
+++ src/app/components/carousel/Slider.tsx
@@ -0,0 +1,13 @@
+type SliderProps = {
+  id: number;
+  text: string[];
+  richtung: string;
+};
+
+export function Slider({ id, text, richtung }: SliderProps) {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
