81f81da47e383464327c744936da737308e20728
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-09-28 19:52:02 +0200

    fix: remove lang attribute from html tag in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html>
       <body className={`${jakartaSans.variable} antialiased`}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
