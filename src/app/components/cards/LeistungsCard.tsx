86d4ee43ba3d1e06943b182a20fb2233f24a4282
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-09-28 15:46:59 +0200

    fix: adjust layout structure in LeistungsCard for improved readability

--- src/app/components/cards/LeistungsCard.tsx
+++ src/app/components/cards/LeistungsCard.tsx
@@ -11,11 +11,15 @@ export function LeistungsCard({
 }: LeistungsCardsProps) {
   return (
     <>
-      <div className="bg-[var(--background-box-color)] border border-white/10 border-[1px] p-8 text-center">
-        <div className="w-72">
-          <img src={image} alt={header} />
-          <h3 className="font-bold text-2xl py-6">{header}</h3>
-          <p className="text-white/70">{subheader}</p>
+      <div className="bg-[var(--background-box-color)] border border-white/10 border-[1px] p-8 text-center ">
+        <div className="">
+          <div>
+            <img src={image} alt={header} />
+          </div>
+          <div>
+            <h3 className="font-bold text-2xl py-6">{header}</h3>
+            <p className="text-white/70">{subheader}</p>
+          </div>
         </div>
       </div>
     </>
