2a1c3000037a61b7ce150c63e85ce19d687186dc
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-01 15:43:46 +0200

    feat: add LeftOrientedHeader component with props for header and subheader

---
+++ src/app/components/headers/LeftOrientedHeader.tsx
@@ -0,0 +1,15 @@
+type LeftOrientedHeaderProps = {
+  subheader: string;
+  header: string;
+};
+
+export function LeftOrientedHeader({
+  subheader,
+  header,
+}: LeftOrientedHeaderProps) {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
