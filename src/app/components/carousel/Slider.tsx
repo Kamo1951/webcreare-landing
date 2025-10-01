c97febb5e16f78ff73063a0602826e5b5b18998b
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-01 15:32:55 +0200

    feat: update Slider component to use string for id and enhance rendering of slides

--- src/app/components/carousel/Slider.tsx
+++ src/app/components/carousel/Slider.tsx
@@ -1,13 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 type SliderProps = {
-  id: number;
+  id: string;
   text: string[];
   richtung: string;
 };
 
 export function Slider({ id, text, richtung }: SliderProps) {
+  const [slides, setSlides] = useState<string[]>([]);
+  useEffect(() => {
+    setSlides([...text, ...text]);
+  }, [text]);
   return (
     <>
-      <div></div>
+      <div className="slider relative w-full h-[var(--slider-height)]" id={id}>
+        <div
+          className={`slider-track absolute t-0 flex ${
+            richtung === "right" ? "scrollRight" : "scrollLeft"
+          }`}
+        >
+          {slides.map((src, index) => (
+            <div
+              className="slide flex text-center justify-center items-center "
+              key={index}
+            >
+              <span>{src}</span>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
