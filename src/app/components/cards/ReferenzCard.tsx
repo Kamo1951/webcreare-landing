3c66d386001e94983fce6eaced9647ba9ca0e1cd
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-02 22:21:29 +0200

    feat: update ReferenzCardProps to accept StaticImageData type for src

--- src/app/components/cards/ReferenzCard.tsx
+++ src/app/components/cards/ReferenzCard.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 type ReferenzCardProps = {
-  src: string;
+  src: string | StaticImageData;
   header: string;
   id: string;
 };
