c81ca3cea0b55bd57c8f6d98f67332c62d218553
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-03 14:19:04 +0200

    feat: add PreiseCard component with props for id, heading, and bodyText

---
+++ src/app/components/cards/PreiseCard.tsx
@@ -0,0 +1,13 @@
+type PreisCardProps = {
+  id: string;
+  heading: string;
+  bodyText: string;
+};
+
+export function PreiseCard(id, heading, bodyText): PreisCardProps {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
