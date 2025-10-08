78629063021dcbff1ab0727971d633ec700a3b83
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-08 15:23:27 +0200

    feat: add TeamCard component for displaying team member information

---
+++ src/app/components/cards/TeamCard.tsx
@@ -0,0 +1,13 @@
+type TeamCardProps = {
+  img: string;
+  name: string;
+  role: string;
+};
+
+export function TeamCard({ img, name, role }: TeamCardProps) {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
