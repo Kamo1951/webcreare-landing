a76e9f7dc1a64b83da518928ca4009decb85ec70
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-08 15:58:48 +0200

    feat: update TeamCard component to use StaticImageData for images and improve rendering structure

--- src/app/components/cards/TeamCard.tsx
+++ src/app/components/cards/TeamCard.tsx
@@ -1,7 +1,10 @@
+import Image, { StaticImageData } from "next/image";
+
 export type TeamItems = {
-  img: string;
+  img: StaticImageData;
   name: string;
   role: string;
+  link: string;
 };
 
 interface TeamCardProps {
@@ -11,21 +14,33 @@ interface TeamCardProps {
 const TeamCard: React.FC<TeamCardProps> = ({ items }) => {
   return (
     <>
-      {items.map((item) => {
-        <div className="flex justify-center gap-x-4">
-          <div className="flex flex-col">
-            <div>
-              {
-                //image dann iwann
-              }
+      <div className="flex justify-center gap-x-4">
+        {items.map((item) => {
+          return (
+            <div key={item.name} className="flex flex-col group">
+              <div className="relative w-60 h-96 overflow-hidden">
+                <Image
+                  src={item.img}
+                  alt={item.name}
+                  fill
+                  className="object-cover"
+                />
+                <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-b from-[var(--accent-color)]/10 to-[var(--accent-color-hover)]/50" />
+              </div>
+              <div className="flex flex-col mt-5">
+                <h5 className="text-semibold text-2xl mb-1 hover:text-[var(--accent-color)]">
+                  <a target="_blank" href={item.link}>
+                    {item.name}
+                  </a>
+                </h5>
+                <p className="text-[var(--paragraph-text-color)] uppercase text-sm font-semibold">
+                  {item.role}
+                </p>
+              </div>
             </div>
-            <div className="flex flex-col">
-              <h5 className="text-semibold">{item.name}</h5>
-              <p className="text-[var(--paragraph-text-color)]">{item.role}</p>
-            </div>
-          </div>
-        </div>;
-      })}
+          );
+        })}
+      </div>
     </>
   );
 };
