0dc5e7c03669f5bd18b919b7f1cf6bd0a27b2d1a
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-08 15:36:23 +0200

    feat: refactor TeamCard component to accept an array of team items for dynamic rendering

--- src/app/components/cards/TeamCard.tsx
+++ src/app/components/cards/TeamCard.tsx
@@ -1,13 +1,33 @@
-type TeamCardProps = {
+export type TeamItems = {
   img: string;
   name: string;
   role: string;
 };
 
-export function TeamCard({ img, name, role }: TeamCardProps) {
+interface TeamCardProps {
+  items: TeamItems[];
+}
+
+const TeamCard: React.FC<TeamCardProps> = ({ items }) => {
   return (
     <>
-      <div></div>
+      {items.map((item) => {
+        <div className="flex justify-center gap-x-4">
+          <div className="flex flex-col">
+            <div>
+              {
+                //image dann iwann
+              }
+            </div>
+            <div className="flex flex-col">
+              <h5 className="text-semibold">{item.name}</h5>
+              <p className="text-[var(--paragraph-text-color)]">{item.role}</p>
+            </div>
+          </div>
+        </div>;
+      })}
     </>
   );
-}
+};
+
+export default TeamCard;
