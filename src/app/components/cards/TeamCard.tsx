9eb92354a2042f8beee6be121a14be08ad517406
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-08 16:29:02 +0200

    fix: update TeamCard component to use item.id as key and improve link accessibility

--- src/app/components/cards/TeamCard.tsx
+++ src/app/components/cards/TeamCard.tsx
@@ -1,6 +1,7 @@
 import Image, { StaticImageData } from "next/image";
 
 export type TeamItems = {
+  id: string | number;
   img: StaticImageData;
   name: string;
   role: string;
@@ -17,7 +18,7 @@ const TeamCard: React.FC<TeamCardProps> = ({ items }) => {
       <div className="flex justify-center gap-x-4">
         {items.map((item) => {
           return (
-            <div key={item.name} className="flex flex-col group">
+            <div key={item.id} className="flex flex-col group">
               <div className="relative w-60 h-96 overflow-hidden">
                 <Image
                   src={item.img}
@@ -28,8 +29,8 @@ const TeamCard: React.FC<TeamCardProps> = ({ items }) => {
                 <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-b from-[var(--accent-color)]/10 to-[var(--accent-color-hover)]/50" />
               </div>
               <div className="flex flex-col mt-5">
-                <h5 className="text-semibold text-2xl mb-1 hover:text-[var(--accent-color)]">
-                  <a target="_blank" href={item.link}>
+                <h5 className="font-semibold text-2xl mb-1 hover:text-[var(--accent-color)]">
+                  <a target="_blank" rel="noopener noreferrer" href={item.link}>
                     {item.name}
                   </a>
                 </h5>
