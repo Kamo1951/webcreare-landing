2abb6e45239e21cb682bb66992dc7e235a8344a9
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-10 16:37:03 +0200

    fix: enhance layout of AchivementBadges component for improved spacing and structure

--- src/app/components/badges/AchievmentBadges.tsx
+++ src/app/components/badges/AchievmentBadges.tsx
@@ -13,12 +13,24 @@ interface AchivementBadgesProps {
 const AchivementBadges: React.FC<AchivementBadgesProps> = ({ items }) => {
   return (
     <>
-      <div className="flex justify-center">
+      <div className="flex justify-center gap-8">
         {items.map((item) => {
           return (
-            <div className="flex place-items-center">
-              <span>{item.svg}</span>
-              <div></div>
+            <div className="flex place-items-center gap-4">
+              <span className="rounded-full bg-[var(--background-box-color)] p-4 ">
+                {item.svg}
+              </span>
+              <div className="flex flex-col">
+                <div className="flex place-items-center text-3xl gap-2">
+                  <p className="text-4xl">{item.number}</p>
+                  <p>{item.numberText}</p>
+                </div>
+                <div className="w-xs">
+                  <p className="text-[var(--paragraph-text-color)]">
+                    {item.bodyText}
+                  </p>
+                </div>
+              </div>
             </div>
           );
         })}
