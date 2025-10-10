979576dcc0b32a648b79ee49b510b720715236eb
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-10 16:46:59 +0200

    fix: remove trailing spaces from number fields for consistency in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -189,7 +189,7 @@ export default function Home() {
           ></path>
         </svg>
       ),
-      number: "5+ ",
+      number: "5",
       numberText: "Jahre Erfahrung",
       bodyText:
         "In diesen 5 Jahren haben wir schon fast jedes Problem gemeistert.",
@@ -278,7 +278,7 @@ export default function Home() {
           ></path>
         </svg>
       ),
-      number: "2 ",
+      number: "2",
       numberText: "Teammitglieder",
       bodyText: "Ein frisches junges Team ist auch ein Schlüssel zum Erfolg.",
     },
