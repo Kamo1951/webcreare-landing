a2429047faadfa00898e28ca6a10db008ef1d345
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-11 12:15:16 +0200

    refactor: remove Navbar and Footer components from RootLayout for cleaner structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import Navbar from "./components/navbar/Navbar";
-import { Footer } from "./components/footer/Footer";
 
 const jakartaSans = Plus_Jakarta_Sans({
   variable: "--font-jakarta",
@@ -32,10 +30,8 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange={true}
         >
-          <Navbar />
           {children}
         </ThemeProvider>
-        <Footer />
       </body>
     </html>
   );
