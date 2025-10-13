a821116f8ab53c95b1f52be2724f96a5faf31381
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-13 16:43:07 +0200

    fix: correct typographical errors in Datenschutzerklärung component text

--- src/app/datenschutzerklaerung/page.tsx
+++ src/app/datenschutzerklaerung/page.tsx
@@ -13,14 +13,14 @@ export default function Datenschutzerklärung() {
               <p className="text-[var(--paragraph-text-color)] leading-relaxed">
                 Mit der folgenden Datenschutzerklärung möchten wir Sie darüber
                 aufklären, welche Arten Ihrer personenbezogenen Daten
-                (nachfolgend auch kurz als "Daten" bezeichnet) wir zu welchen
+                (nachfolgend auch kurz als „Daten“ bezeichnet) wir zu welchen
                 Zwecken und in welchem Umfang verarbeiten. Die
                 Datenschutzerklärung gilt für alle von uns durchgeführten
                 Verarbeitungen personenbezogener Daten, sowohl im Rahmen der
                 Erbringung unserer Leistungen als auch insbesondere auf unseren
                 Webseiten, in mobilen Applikationen sowie innerhalb externer
                 Onlinepräsenzen, wie z. B. unserer Social-Media-Profile
-                (nachfolgend zusammenfassend bezeichnet als "Onlineangebot").
+                (nachfolgend zusammenfassend bezeichnet als „Onlineangebot“).
               </p>
               <p className="text-[var(--paragraph-text-color)] leading-relaxed">
                 Die verwendeten Begriffe sind nicht geschlechtsspezifisch.
