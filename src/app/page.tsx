c34c35a5afdda3636ee6dbb4dd69dc7657fbfd7b
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-15 22:35:24 +0200

    feat: update team member link for improved navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -191,7 +191,7 @@ export default function Home() {
       img: OliPort,
       name: "Oliwer Keskin",
       role: "Vertrieb",
-      link: "",
+      link: "https://webcreare.de/#team",
     },
   ];
 
