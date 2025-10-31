373a9e7adc3b3766e958d33ef3b63a71496abb23
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-31 14:01:15 +0100

    feat: update grid layout in Referenzen page for improved responsiveness

--- src/app/referenzen/page.tsx
+++ src/app/referenzen/page.tsx
@@ -24,7 +24,7 @@ export default function Referenzen() {
           direction="center"
           widthSize="w-md"
         />
-        <div className="grid grid-cols-2 grid-rows-2 place-content-center gap-8 px-20">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 px-4 md:px-10 lg:px-20">
           <ReferenzCard
             src={Speckert}
             mobileSrc={BausuchtMobile}
