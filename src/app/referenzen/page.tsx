c7e28b069ee3ec943ea6f99b67e4ec9261cbe8a3
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-31 13:51:25 +0100

    feat: add SubAndMainHeader and ReferenzCard components to Referenzen page

--- src/app/referenzen/page.tsx
+++ src/app/referenzen/page.tsx
@@ -1,4 +1,9 @@
+//components
 import Navbar from "../components/navbar/Navbar";
+import { SubAndMainHeader } from "../components/headers/SubAndMainHeader";
+import { ReferenzCard } from "../components/cards/ReferenzCard";
+
+//desktop images
 import Veerdesign from "../components/imgs/Veerdesign.webp";
 import Speckert from "../components/imgs/Speckert.webp";
 import Bausucht from "../components/imgs/Bausucht.webp";
@@ -12,7 +17,30 @@ export default function Referenzen() {
   return (
     <>
       <Navbar />
-      <div></div>
+      <div>
+        <SubAndMainHeader
+          subheader="Referenzen"
+          header="Unsere aktuellsten Projekte"
+          direction="center"
+          widthSize="w-md"
+        />
+        <div className="flex flex-col sm:flex-row place-content-center gap-8 px-4">
+          <ReferenzCard
+            src={Speckert}
+            mobileSrc={BausuchtMobile}
+            header="E-commerce für Kunst"
+            id="01"
+            url="https://www.speckert.net/"
+          />
+          <ReferenzCard
+            src={Veerdesign}
+            mobileSrc={VeerdesignMobile}
+            header="Buchsverkaufs Webseite"
+            id="02"
+            url="https://veerdesign.de/"
+          />
+        </div>
+      </div>
     </>
   );
 }
