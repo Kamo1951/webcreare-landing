653f3f88eb11786506666fc8682090a978d57564
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-10-31 13:48:44 +0100

    feat: refactor Referenzen component structure and improve layout

--- src/app/referenzen/page.tsx
+++ src/app/referenzen/page.tsx
@@ -1,7 +1,18 @@
+import Navbar from "../components/navbar/Navbar";
+import Veerdesign from "../components/imgs/Veerdesign.webp";
+import Speckert from "../components/imgs/Speckert.webp";
+import Bausucht from "../components/imgs/Bausucht.webp";
+
+//mobile images
+import VeerdesignMobile from "../components/navbar/imgs/mobile imgs/Veerdesign.webp";
+import SpeckertMobile from "../components/navbar/imgs/mobile imgs/Veerdesign.webp";
+import BausuchtMobile from "../components/navbar/imgs/mobile imgs/Bausucht.webp";
+
 export default function Referenzen() {
   return (
-    <div>
+    <>
+      <Navbar />
       <div></div>
-    </div>
+    </>
   );
 }
