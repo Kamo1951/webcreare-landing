8bf21afb39560f097c026d373b9cf9c2f1390263
Mikolaj Spruch <mikolajspruch@gmail.com>
2025-11-07 21:17:16 +0100

    feat: update layout of contact information in Home component for better alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -723,7 +723,7 @@ export default function Home() {
         </section>
       </main>
 
-      <div className="my-50 flex justify-center font-xl">
+      <div className="my-50 flex justify-center font-xl flex-col">
         <p>www.webcreare.de</p>
         <br />
         <br />
